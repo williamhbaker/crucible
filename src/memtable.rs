f7e66c7a162480e82c1b06b7d68bb34cbad9c661
Will Baker <wbaker@gmail.com>
2023-04-18 19:08:44 -0400

    refactor(memtable): explicitly track deleted records as None

--- src/memtable.rs
+++ src/memtable.rs
@@ -1,9 +1,10 @@
-use std::collections::{self, HashMap};
+use std::collections::HashMap;
 
 use crate::wal;
 
 pub struct MemTable {
-    data: HashMap<Vec<u8>, Vec<u8>>,
+    // A that is present in the HashMap with a value of None represents a specific deletion record.
+    data: HashMap<Vec<u8>, Option<Vec<u8>>>,
 }
 
 impl MemTable {
@@ -14,17 +15,23 @@ impl MemTable {
     }
 
     pub fn put(&mut self, key: &[u8], val: &[u8]) {
-        self.data.insert(key.to_vec(), val.to_vec());
+        self.data.insert(key.to_vec(), Some(val.to_vec()));
     }
 
     pub fn get(&self, key: &[u8]) -> Option<&Vec<u8>> {
-        self.data.get(key)
+        if let Some(val) = self.data.get(key) {
+            // HashMap contains a record for this key, but the value might still be None if it was
+            // from a deletion.
+            val.as_ref()
+        } else {
+            // No record for this key. We have no knowledge of it ever existing or having been
+            // deleted.
+            None
+        }
     }
 
     pub fn del(&mut self, key: &[u8]) {
-        if let collections::hash_map::Entry::Occupied(o) = self.data.entry(key.to_vec()) {
-            o.remove_entry();
-        }
+        self.data.insert(key.to_vec(), None);
     }
 }
 
