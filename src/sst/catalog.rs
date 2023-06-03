e591e3877ebc88c53f7e9832b8eaefc703ef951d
Will Baker <wbaker@gmail.com>
2023-06-02 21:01:51 -0400

    feat(catalog): handle tables in leveled directories

--- src/sst/catalog.rs
+++ src/sst/catalog.rs
@@ -10,46 +10,76 @@ use crate::protocol::{ReadRecord, WriteRecord, SST_EXT};
 use super::{table_sequence, Table};
 
 pub struct Catalog {
-    ssts: Vec<Table>,
+    ssts: Vec<Vec<Table>>, // Index 0 is level 0, 1 is 1, etc.
     watermark: u32,
     data_dir: path::PathBuf,
 }
 
 impl Catalog {
     pub fn new(data_dir: &path::Path) -> io::Result<Self> {
-        let mut watermark = 0;
-
-        // TODO: This should descend to sub-dirs and take that into account when calculating
-        // sequence, since higher-level (older) tables will not have a sequence.
-        let mut ssts = fs::read_dir(data_dir)?
+        let mut dirs = fs::read_dir(data_dir)?
             .into_iter()
             .collect::<io::Result<Vec<fs::DirEntry>>>()?
-            .iter()
-            .filter_map(|entry| {
-                let path = entry.path();
+            .into_iter()
+            .filter(|entry| entry.path().is_dir())
+            .collect::<Vec<fs::DirEntry>>();
 
-                if !path.is_dir() {
-                    if let Some(ext) = path.extension() {
-                        if ext.eq_ignore_ascii_case(SST_EXT) {
+        // Directories will be sorted ascending by the integer value of their name. Each of these
+        // directories represents a compaction level. Level 0 is special and contains the flushed
+        // memtables that have not undergone any compaction: These tables will have overlapping key
+        // ranges. Tables at higher levels will not have overlapping key ranges.
+        dirs.sort_unstable_by_key(|dir| {
+            dir.file_name()
+                .to_str()
+                .expect("must convert dir name to string")
+                .parse::<usize>()
+                .expect("must parse dir name as usize")
+        });
+
+        let mut watermark = 0;
+        let mut ssts = vec![];
+
+        for (level, dir) in dirs.iter().enumerate() {
+            let mut these_ssts = vec![];
+
+            let files = fs::read_dir(dir.path()).unwrap();
+
+            let mut files = files
+                .into_iter()
+                .collect::<io::Result<Vec<fs::DirEntry>>>()
+                .unwrap()
+                .into_iter()
+                .filter(|file| file.path().is_file())
+                .collect::<Vec<fs::DirEntry>>();
+
+            files.sort_unstable_by_key(|file| {
+                file.path()
+                    .file_stem()
+                    .unwrap()
+                    .to_string_lossy()
+                    .parse::<usize>()
+                    .unwrap()
+            });
+
+            files.into_iter().for_each(|file| {
+                let path = file.path();
+                if let Some(ext) = path.extension() {
+                    if ext.eq_ignore_ascii_case(SST_EXT) {
+                        if level == 0 {
                             // Parse the file stem (name without extension) to update the watermark.
+                            // The files are already sorted in ascending order.
                             if let Some(seq) = table_sequence(&path) {
-                                if seq > watermark {
-                                    watermark = seq;
-                                }
+                                watermark = seq;
                             }
-
-                            return Some(Table::new(&path));
                         }
+
+                        these_ssts.push(Table::new(&path).unwrap());
                     }
                 }
+            });
 
-                None
-            })
-            .collect::<io::Result<Vec<Table>>>()?;
-
-        // Tables must be sorted oldest to newest (ascending sequence). The newest tables will be
-        // queried first on lookups. Newer tables have a higher sequence number.
-        ssts.sort_unstable_by_key(|t| t.sequence);
+            ssts.push(these_ssts);
+        }
 
         Ok(Catalog {
             ssts,
@@ -59,9 +89,13 @@ impl Catalog {
     }
 
     pub fn get(&self, key: &[u8]) -> io::Result<Option<ReadRecord>> {
-        for sst in self.ssts.iter().rev() {
-            if let Some(rec) = sst.get(key)? {
-                return Ok(Some(rec));
+        // Start at the lowest level (newest data) and check newest to oldest tables for the record.
+        // The first one found is returned.
+        for level in self.ssts.iter() {
+            for sst in level.iter().rev() {
+                if let Some(rec) = sst.get(key)? {
+                    return Ok(Some(rec));
+                }
             }
         }
 
@@ -75,7 +109,11 @@ impl Catalog {
         let mut sorted_records: Vec<WriteRecord> = records.into_iter().collect();
         sorted_records.sort_unstable_by_key(|v| v.key().to_vec());
 
-        let mut path: path::PathBuf = path::PathBuf::from(&self.data_dir);
+        // Flush to level 0 exclusively.
+        let mut path: path::PathBuf = path::PathBuf::from(&self.data_dir).join("0");
+        // The level 0 directory may not exist yet.
+        fs::create_dir_all(&path)?;
+
         path = path.join(format!("{}", &self.watermark + 1));
         path.set_extension(SST_EXT);
 
@@ -115,9 +153,12 @@ impl Catalog {
         // writing it.
         let new = Table::new(&path)?;
 
-        // Add the new table, which must be the highest numbered, to the end of the list of tables.
-        // This preserves the requirement that the tables be in order of oldest to newest.
-        self.ssts.push(new);
+        // Add the new table, which must be the highest numbered, to the end of the list of level 0
+        // tables. This preserves the requirement that the tables be in order of oldest to newest.
+        if self.ssts.len() == 0 {
+            self.ssts.push(Vec::new());
+        }
+        self.ssts[0].push(new);
 
         self.watermark += 1;
 
